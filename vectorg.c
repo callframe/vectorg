42c1c031d525dbf3b028d128eba3121c75318584
Fabrice <fabrice@schaub-dev.xyz>
2026-07-11 11:05:40 +0200

    writing image

--- vectorg.c
+++ vectorg.c
@@ -1,8 +1,17 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
+
+#if !defined(FB_HEIGHT)
+#define FB_HEIGHT 1080
+#endif
+
+#if !defined(FB_WIDTH)
+#define FB_WIDTH 1920
+#endif
 
 int main(int argc, char* argv[])
 {
@@ -12,11 +21,13 @@ int main(int argc, char* argv[])
     return EXIT_FAILURE;
   }
 
-  FILE* fp = fopen(argv[1], "wb+");
-  if(fp == NULL) {
-    fprintf(stderr, "failed to open file at %s\n", argv[1]);
+  uint32_t* fb = aligned_alloc(_Alignof(uint32_t), sizeof(uint32_t) * (FB_WIDTH * FB_HEIGHT));
+  if (fb == NULL)
+  {
+    fprintf(stderr, "failed to allocate framebuffer of size %dx%d\n", FB_WIDTH, FB_HEIGHT);
     return 1;
   }
 
-  fclose(fp);
+  stbi_write_png(argv[1], FB_WIDTH, FB_HEIGHT, sizeof(uint32_t), fb, FB_WIDTH * sizeof(uint32_t));
+  free(fb);
 }
